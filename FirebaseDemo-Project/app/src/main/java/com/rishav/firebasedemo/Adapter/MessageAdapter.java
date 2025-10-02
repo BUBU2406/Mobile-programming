2c5faf8913fce9a7130b621be16c8b25784371ec
BUBU <huynhthihoanghao2406@gmail.com>
2025-10-02 16:11:16 +0700

    A SOCIAL NETWORK ON ANDROID

---
+++ FirebaseDemo-Project/app/src/main/java/com/rishav/firebasedemo/Adapter/MessageAdapter.java
@@ -0,0 +1,4 @@
+package com.rishav.firebasedemo.Adapter;
+
+public class MessageAdapter {
+}
---
+++ FirebaseDemo-Project/app/src/main/java/com/rishav/firebasedemo/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.rishav.firebasedemo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+
+
+}
